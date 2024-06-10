b646d1ffe434a2e47d462c7221120a9bd3591a76
crisdesivo <cdesivo92@gmail.com>
2024-06-10 02:34:54 -0300

    collectstatic

--- CristianDesivo/settings.py
+++ CristianDesivo/settings.py
@@ -88,9 +88,9 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = []
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
