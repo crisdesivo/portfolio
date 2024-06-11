79e5b3a86e8dd11b7b1fa1b2566f253824b42e28
crisdesivo <cdesivo92@gmail.com>
2024-06-11 02:47:31 -0300

    Added project pages and more projects

--- personal/models.py
+++ personal/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Project(models.Model):
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
+    markdown = models.TextField(blank=True)
     link = models.URLField(blank=True)
     image = models.ImageField(upload_to='images/', blank=True)
 
--- personal/views.py
+++ personal/views.py
@@ -2,6 +2,12 @@
 from django.shortcuts import render
 from .models import Project
 
+# make views for individual projects
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'personal/project_detail.html', {'project': project})
+
+# show all projects
 def home(request):
     projects = Project.objects.all()
     return render(request, 'personal/home.html', {'projects': projects})
