035bd2b6de32a94b2c18ec785d798f8e5270911d
crisdesivo <cdesivo92@gmail.com>
2024-06-18 13:39:25 -0300

    lists

--- personal/admin.py
+++ personal/admin.py
@@ -1,5 +1,7 @@
 # Register your models here.
 from django.contrib import admin
 from .models import Project
+from .models import RecursiveList
 
 admin.site.register(Project)
+admin.site.register(RecursiveList)
