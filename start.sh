d0798c71e6e4d7515e0d1c8c9b5cc1596a8486fb
crisdesivo <cdesivo92@gmail.com>
2024-11-12 15:40:32 -0300

    updated gunicorn command

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-gunicorn --bind :${PORT} CristianDesivo.wsgi:application
+gunicorn --bind :${PORT} CristianDesivo.wsgi:application \
+    --workers 2 \
+    --threads 4 \
+    --timeout 60 \
+    --access-logfile - \
+    --error-logfile -
